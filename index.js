fb84f8df88bd80fe366fd9dc0dc9b1b2c38abfde
army8735 <army8735@qq.com>
2015-02-04 19:09:52 +0800

    编译css

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ var path = require('path');
 var fs = require('fs');
 var More = require('more-css');
 
-module.exports = function (dir, option) {
+module.exports = function(dir, option) {
   dir = dir || process.cwd();
   option = option || {};
 
+  var pkg = path.join(dir, 'package.json');
+
   return function * (next) {
+    if(!fs.existsSync(pkg)) {
+      yield * next;
+    }
+
+    var pkgJson = fs.readFileSync(pkg, { encoding: 'utf-8' });
+    pkgJson = JSON.parse(pkgJson);
+
     var isCss = /\.css($|\?)/.test(this.url);
-    if(isCss) {
+    if(isCss && pkgJson.spm && pkgJson.spm.moreOpts) {
       var content = this.body;
       if(!content) {
         var file = path.join(dir, this.url);
@@ -17,9 +26,26 @@ module.exports = function (dir, option) {
         }
         content = fs.readFileSync(file, 'utf-8');
       }
+
       var more = new More();
+      var moreOpts = pkgJson.spm.moreOpts;
+      if(moreOpts.kw) {
+        more.addKeyword(moreOpts.kw);
+      }
+      if(moreOpts.configFile) {
+        var cf;
+        if(moreOpts.configFile.charAt(0) === '.') {
+          cf = path.join(dir, moreOpts.configFile);
+        }
+        else {
+          cf = moreOpts.configFile;
+        }
+        more.configFile(cf);
+      }
+
       var s = more.parse(content);
       this.body = s;
+
       if(option.next && option.next.call(this)) {
         yield * next;
       }
